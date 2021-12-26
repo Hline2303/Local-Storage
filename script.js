79876191918d2ea7e282015767e3f328cc419ba1
Solène Médina <solene.medina.pro@gmailcom>
2021-12-26 17:34:10 +0100

    Démo découverte Local Storage

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('connecté !');
+console.log("connecté !");
+
+// Je crée un stockage sous la forme clé/valeur
+// localStorage.setItem("nom", "Durand");
+
+// Je vérifie le contenu de localStorage
+// const stockage = window.localStorage;
+// console.log(stockage);
+
+// Je récupère le contenu du stockage 'nom
+console.log(localStorage.getItem('nom'));
